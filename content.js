9944a79528c15703beecd19bfd807601219a9474
Michael Villena <mvillena3@gatech.edu>
2012-09-24 11:51:33 -0400

    Reverted back to GET request for extension

--- content.js
+++ content.js
@@ -3,35 +3,43 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     code: "chrome.extension.sendRequest({selection: window.getSelection().toString(), url: document.URL});"});
 }); 
 
-function fakePost(text, link) {   
-    var form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", "http://www.worth-reading.org/chrome_extension/new");
-    form.setAttribute("accept-charset", "UTF-8");
-    var params = {text: text, link: link};
-    for(var key in params) {
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
-        form.appendChild(hiddenField);
-    }
-    document.body.appendChild(form);
-    form.submit();
-};
+// function fakePost(text, link) {   
+//     var form = document.createElement("form");
+//     form.setAttribute("method", "post");
+//     form.setAttribute("action", "http://www.worth-reading.org/chrome_extension/new");
+//     form.setAttribute("accept-charset", "UTF-8");
+//     var params = {text: text, link: link};
+//     for(var key in params) {
+//         var hiddenField = document.createElement("input");
+//         hiddenField.setAttribute("type", "hidden");
+//         hiddenField.setAttribute("name", key);
+//         hiddenField.setAttribute("value", params[key]);
+//         form.appendChild(hiddenField);
+//     }
+//     document.body.appendChild(form);
+//     form.submit();
+// };
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.selection) {
-    fakePostCode = fakePost.toString().replace(/(\n|\t)/gm, '');
+    // fakePostCode = fakePost.toString().replace(/(\n|\t)/gm, '');
     chrome.tabs.query({'url': 'http://www.worth-reading.org/chrome_extension/*'}, function(tab) {
       if (tab[0]) {
-        chrome.tabs.update(tab[0].id, 
-          { 'url': "javascript:" + fakePostCode + "; fakePost('" + request.selection + "', '" + request.url + "');",
-            'active': true
+        chrome.tabs.update(tab[0].id,  {
+           'url': 'http://worth-reading.org/chrome_extension/new?text=' + request.selection 
+           + '&link=' + request.url 
+
+        //  { 'url': "javascript:" + fakePostCode + "; fakePost('" 
+        //  + request.selection + "', '" + request.url + "');",
+        //    'active': true
         });
       } else {
         chrome.tabs.create({
-          'url': "javascript:" + fakePostCode + "; fakePost('" + request.selection + "', '" + request.url + "');"
+           'url': 'http://worth-reading.org/chrome_extension/new?text=' + request.selection 
+           + '&link=' + request.url 
+
+        //  'url': "javascript:" + fakePostCode + "; fakePost('" 
+        //  + request.selection + "', '" + request.url + "');"
         });
 
         // Development mode 
